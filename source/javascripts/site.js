1f3d1f4e99a87f1b2c6cada7f615498ca9eb3084
kimiko ando <kimiko.ando13@icloud.com>
2023-12-16 00:18:14 +0900

    edited partials and css

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
 // This is where it all goes :)
+function switchMode() {
+  let moon = document.getElementById ("moon");
+    if(moon.className=="moon"){
+      moon.className="sun";
+      document.body.style.backgroundColor = "#141D26";
+      document.body.style.color = "#fff";
+    }
+  else {
+    moon.className ="moon";
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+  }
+}
